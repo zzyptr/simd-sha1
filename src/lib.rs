6bc48ccaa8ea359e7e0431d24ff9979e4fab8d75
Jon <zzyptr@gmail.com>
2023-11-08 22:42:00 +0800

    Rename

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@ use std::arch::x86_64::*;
 /// ```
 /// let data = [];
 /// 
-/// let digest = simd_sha1::sha1(&data);
+/// let digest = simd_sha1::hash(&data);
 /// let expect = [
 ///    0xda, 0x39, 0xa3, 0xee, 0x5e, 0x6b, 0x4b, 0x0d, 0x32, 0x55, 
 ///    0xbf, 0xef, 0x95, 0x60, 0x18, 0x90, 0xaf, 0xd8, 0x07, 0x09
@@ -21,7 +21,7 @@ use std::arch::x86_64::*;
 /// 
 /// assert_eq!(digest, expect);
 /// ```
-pub fn sha1(bytes: &[u8]) -> [u8; 20] {
+pub fn hash(bytes: &[u8]) -> [u8; 20] {
     let original_length = bytes.len();
     let zero_count = ((original_length + 64) & !63) - original_length - 9;
     
@@ -228,7 +228,7 @@ mod tests {
     #[test]
     fn test_sha1() {
         let data = "The quick brown fox jumps over the lazy dog".as_bytes();
-        let digest = sha1(data);
+        let digest = hash(data);
         let expect = [
             0x2f, 0xd4, 0xe1, 0xc6, 0x7a, 0x2d, 0x28, 0xfc, 0xed, 0x84, 
             0x9e, 0xe1, 0xbb, 0x76, 0xe7, 0x39, 0x1b, 0x93, 0xeb, 0x12
@@ -236,7 +236,7 @@ mod tests {
         assert_eq!(digest, expect);
 
         let data = "The quick brown fox jumps over the lazy cog".as_bytes();
-        let digest = sha1(data);
+        let digest = hash(data);
         let expect = [
             0xde, 0x9f, 0x2c, 0x7f, 0xd2, 0x5e, 0x1b, 0x3a, 0xfa, 0xd3, 
             0xe8, 0x5a, 0x0b, 0xd1, 0x7d, 0x9b, 0x10, 0x0d, 0xb4, 0xb3
